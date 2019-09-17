b8781571f65e46030dbfab4d25bb73b5c8a7c1e0
Diego Menezes <diegomenezes21@hotmail.com>
2019-09-17 10:36:10 -0300

    Criando o projeto Atividade04

--- Atividade04/src/app/app-routing.module.ts
+++ Atividade04/src/app/app-routing.module.ts
@@ -4,6 +4,13 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: 'header', loadChildren: './header/header.module#HeaderPageModule' },
+  { path: 'footer', loadChildren: './footer/footer.module#FooterPageModule' },
+  { path: 'tabs', loadChildren: './tabs/tabs.module#TabsPageModule' },
+  { path: 'menu', loadChildren: './menu/menu.module#MenuPageModule' },
+  { path: 'split', loadChildren: './split/split.module#SplitPageModule' },
+  { path: 'grid', loadChildren: './grid/grid.module#GridPageModule' },
+  { path: 'list', loadChildren: './list/list.module#ListPageModule' },
 ];
 
 @NgModule({
